23e03a2cb5489cea59eb8c04f0215045474aafd2
Florian Kleewein <floriankl@edu.aau.at>
2020-12-08 14:18:28 +0100

    first tests

--- src/main/java/ab1/impl/Sandner_Kleewein_Oraze/RMImpl.java
+++ src/main/java/ab1/impl/Sandner_Kleewein_Oraze/RMImpl.java
@@ -5,9 +5,17 @@ import ab1.RM;
 import java.util.List;
 
 public class RMImpl implements RM {
+
+	private int[] memoryContent;
+	private int accumulator;
+
+	public RMImpl(){
+		this.memoryContent = new int[0];
+	}
+
 	@Override
 	public void setInitialMemoryContent(int[] content) {
-
+		this.memoryContent = content;
 	}
 
 	@Override
@@ -17,6 +25,18 @@ public class RMImpl implements RM {
 
 	@Override
 	public int[] getMemoryContent() {
-		return new int[0];
+		int count = 0;
+		for(int i = memoryContent.length-1; i>=0; i--){
+			if(memoryContent[i] == 0){
+				count++;
+			}else{
+				break;
+			}
+		}
+		int[] newArr = new int[memoryContent.length - count];
+		for(int i = 0; i<newArr.length; i++){
+			newArr[i] = memoryContent[i];
+		}
+		return newArr;
 	}
 }
