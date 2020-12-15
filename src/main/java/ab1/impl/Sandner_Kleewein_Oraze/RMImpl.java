8bb6d575bca52521fbcbd554187b65b6624ace2c
Fabian Oraze <oraze.fabian@icloud.com>
2020-12-15 15:58:44 +0100

    finalized register machine

--- src/main/java/ab1/impl/Sandner_Kleewein_Oraze/RMImpl.java
+++ src/main/java/ab1/impl/Sandner_Kleewein_Oraze/RMImpl.java
@@ -1,14 +1,19 @@
 package ab1.impl.Sandner_Kleewein_Oraze;
 
 import ab1.RM;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RMImpl implements RM {
 
     private int[] memoryContent;
+    private Map<String, Integer> labels;
 
     public RMImpl() {
         this.memoryContent = new int[0];
+        this.labels = new HashMap<>();
     }
 
     @Override
@@ -18,18 +23,34 @@ public class RMImpl implements RM {
 
     @Override
     public boolean execute(List<String> programm) {
-
+        // initial loop to determine labels
+        for (int i = 0; i < programm.size(); i++) {
+            checkForLabel(programm, i);
+        }
+        if (this.memoryContent.length == 0) {
+            this.memoryContent = new int[]{0};
+        }
         for (int i = 0; i < programm.size(); i++) {
             i = checkCommand(programm.get(i), i, programm.size());
         }
         return true;
     }
 
+    private void checkForLabel(List<String> program, int currPos) {
+        if (program.get(currPos).contains(":")) {
+            String[] arr = program.get(currPos).split(":");
+            String label = arr[0];
+            String cmd = arr[1].substring(1);
+            this.labels.put(label, currPos);
+            program.set(currPos, cmd);
+        }
+    }
+
     private int checkCommand(String x, int index, int amountOfCommands) {
         String[] temp = x.split(" ");
         String command = temp[0];
 
-        if (command.toLowerCase().equals("end")) {
+        if (command.equalsIgnoreCase("end")) {
             return amountOfCommands;
         }
         String param = temp[1];
@@ -79,16 +100,23 @@ public class RMImpl implements RM {
         for (int i = 0; i < newArr.length; i++) {
             newArr[i] = memoryContent[i];
         }
+
         return newArr;
     }
 
     private int getParamValue(String param) {
-        if (param.contains("#")) {
-            param = param.substring(1);
-            return Integer.parseInt(param);
+        try {
+            if (param.startsWith("#")) {
+                param = param.substring(1);
+                return Integer.parseInt(param);
+            } else if (param.startsWith("*")) {
+                param = param.substring(1);
+                return memoryContent[memoryContent[Integer.parseInt(param)]];
+            }
+            return memoryContent[Integer.parseInt(param)];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return 0;
         }
-        return memoryContent[Integer.parseInt(param)];
-
     }
 
     private void execLoad(String param) {
@@ -97,20 +125,28 @@ public class RMImpl implements RM {
     }
 
     private void execStore(String param) {
-        int paramvalue = Integer.parseInt(param); //TODO: with * aswell?
-        if (paramvalue > memoryContent.length - 1) {
-            int[] newArr = new int[paramvalue + 1];
+        int paramValue = getParamValueStore(param);
+        if (paramValue > memoryContent.length - 1) {
+            int[] newArr = new int[paramValue + 1];
             for (int i = 0; i < memoryContent.length; i++) {
                 newArr[i] = memoryContent[i];
             }
             for (int i = memoryContent.length; i < newArr.length; i++) {
                 newArr[i] = 0;
             }
-            newArr[paramvalue] = memoryContent[0];
+            newArr[paramValue] = memoryContent[0];
             memoryContent = newArr;
         } else {
-            memoryContent[paramvalue] = memoryContent[0];
+            memoryContent[paramValue] = memoryContent[0];
         }
+    }
+
+    private int getParamValueStore(String param) {
+        if (param.startsWith("*")) {
+            param = param.substring(1);
+            return memoryContent[Integer.parseInt(param)];
+        }
+        return Integer.parseInt(param);
     }
 
     private void execAdd(String param) {
@@ -136,12 +172,18 @@ public class RMImpl implements RM {
 
     private int execJzero(String param, int index) {
         if (memoryContent[0] == 0) {
+            if (this.labels.containsKey(param)) {
+                return this.labels.get(param) - 1;
+            }
             return Integer.parseInt(param) - 2;
         }
         return index;
     }
 
     private int execGoto(String param) {
+        if (this.labels.containsKey(param)) {
+            return this.labels.get(param) - 1;
+        }
         return Integer.parseInt(param) - 2;
     }
 
